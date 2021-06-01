d9e755f82ab5c64a51388bf7c766839e01c3cd06
lukas <lukas.block@pm.me>
2021-06-01 23:42:35 +0200

    add reitit

--- src/clj/lhrb/server.clj
+++ src/clj/lhrb/server.clj
@@ -1,8 +1,15 @@
 (ns lhrb.server
   (:require [io.pedestal.http :as server]
-            [io.pedestal.http.route :as route]
             [io.pedestal.test :as test]
-            [io.pedestal.http.body-params :as params]
+            [reitit.pedestal :as pedestal]
+            [reitit.http :as http]
+            [reitit.ring :as ring]
+            [reitit.dev.pretty :as pretty]
+            [reitit.http.interceptors.parameters :as parameters]
+            [reitit.http.interceptors.muuntaja :as muuntaja]
+            [reitit.http.interceptors.exception :as exception]
+            [reitit.http.interceptors.multipart :as multipart]
+            [muuntaja.core :as m]
             [lhrb.io :refer [disk->edn]]
             [lhrb.engine :refer [q]]))
 
@@ -20,8 +27,7 @@
    :enter
    (fn [context]
      (let [request (:request context)
-           response (ok context)]
-       (def ctx context)
+           response (ok request)]
        (assoc context :response (ok response))))})
 
 (def home
@@ -42,17 +48,41 @@
                   ;; TODO use transit instead
                   "Content-Type" "application/edn"))))})
 
+(defn interceptor [number]
+  {:enter (fn [ctx] (update-in ctx [:request :number] (fnil + 0) number))})
+
 (def routes
-  (route/expand-routes
+  (pedestal/routing-interceptor
+    (http/router
+     [["/ping" {:get {:handler (fn [req]
+                                 {:status 200 :body "hi"})}}]]
+
+      {:exception pretty/exception
+       :data {:muuntaja m/instance
+              :interceptors [(parameters/parameters-interceptor)
+                             (muuntaja/format-negotiate-interceptor)
+                             (muuntaja/format-response-interceptor)
+                             (exception/exception-interceptor)
+                             (muuntaja/format-request-interceptor)
+                             (multipart/multipart-interceptor)]}})
+    (ring/routes
+     (ring/create-resource-handler {:path "/"})
+     (ring/create-default-handler))))
+
+#_(def routes
+  [["/" {:get {:interceptors [home]}}]
+   ["/repl" {:post {:interceptors [(params/body-params) repl]}}]]
+  #_(route/expand-routes
    #{["/" :get home :route-name :home]
      ["/repl" :post [(params/body-params) repl] :route-name :repl]}))
 
 (def service-map
-  {::server/routes routes
+  {::server/routes []
    ::server/type   :jetty
    ::server/port   8890
-   ::server/resource-path "/public"
-   ::server/secure-headers {:content-security-policy-settings {:object-src "none"}}})
+  ; ::server/resource-path  "public"
+   ::server/secure-headers {:content-security-policy-settings
+                            {:object-src "none"}}})
 
 (defn start []
   (server/start (server/create-server service-map)))
@@ -67,11 +97,12 @@
                       ::server/join? false
                       ;; Routes can be a function that resolve routes,
                       ;;  we can use this to set the routes to be reloadable
-                      ::server/routes #(deref #'routes)
+                      ;;::server/routes #(deref #'routes)
                       ;; all origins are allowed in dev mode
                       ::server/allowed-origins {:creds true :allowed-origins (constantly true)}})
               ;; Wire up interceptor chains
               server/default-interceptors
+              (pedestal/replace-last-interceptor routes)
               server/dev-interceptors
               server/create-server
               server/start)))
@@ -105,6 +136,10 @@
 
   (eval '(load-db db2 "resources/got-db.edn"))
 
+  (test/response-for (:io.pedestal.http/service-fn @lhrb.server/server)
+                     :get "/ping"
+                     :header {"Content-Type" "text/plain"}
+                     :body "hallo")
 
   (get-in ctx [:request :edn-params])
 
