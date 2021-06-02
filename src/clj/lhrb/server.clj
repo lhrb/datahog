d4ffdd451d7b9cd5a6bd4fbb1a2c096020e47785
lukas <lukas.block@pm.me>
2021-06-02 23:03:20 +0200

    fully integrate reitit
    * major headache: response map {:body (eval req)} did throw
    exceptions, took a while to figure out what was going on, since
    this worked before I switched to reitit/transit.
    Fix: eval in let form and wrap the result in a map,

--- src/clj/lhrb/server.clj
+++ src/clj/lhrb/server.clj
@@ -1,15 +1,18 @@
 (ns lhrb.server
   (:require [io.pedestal.http :as server]
             [io.pedestal.test :as test]
-            [reitit.pedestal :as pedestal]
-            [reitit.http :as http]
             [reitit.ring :as ring]
+            [reitit.http :as http]
             [reitit.dev.pretty :as pretty]
             [reitit.http.interceptors.parameters :as parameters]
             [reitit.http.interceptors.muuntaja :as muuntaja]
             [reitit.http.interceptors.exception :as exception]
             [reitit.http.interceptors.multipart :as multipart]
+            ;; Uncomment to use
+            ; [reitit.http.interceptors.dev :as dev]
+            [reitit.pedestal :as pedestal]
             [muuntaja.core :as m]
+
             [lhrb.io :refer [disk->edn]]
             [lhrb.engine :refer [q]]))
 
@@ -17,18 +20,18 @@
   (let [db (disk->edn path)]
    `(def ~db-name ~db)))
 
-(def routes
+(def router
   (pedestal/routing-interceptor
     (http/router
      [["/ping" {:get {:handler (fn [req]
-                                 {:status 200 :body "hi"})}}]
+                                 {:status 200 :body "hallo"})}}]
       ["/repl" {:post
-                {:handler (fn [{:keys [body-params]}]
-                            {:status 200
-                             :headers {"Content-Type" "application/transit+json"}
-                             :body (eval body-params)})}}]]
-
-      {:exception pretty/exception
+                {:handler (fn [req]
+                            (let [res (eval (:body-params req))]
+                             {:status 200
+                              :headers {"Content-Type" "application/transit+json"}
+                              :body {:result res}}))}}]]
+     {:exception pretty/exception
        :data {:muuntaja m/instance
               :interceptors [(parameters/parameters-interceptor)
                              (muuntaja/format-negotiate-interceptor)
@@ -44,9 +47,12 @@
   {::server/routes []
    ::server/type   :jetty
    ::server/port   8890
-  ; ::server/resource-path  "public"
    ::server/secure-headers {:content-security-policy-settings
-                            {:object-src "none"}}})
+                            {:object-src "'none'"}
+                            #_{:default-src "'self'"
+                             :style-src "'self' 'unsafe-inline'"
+                             :script-src "'self' 'unsafe-inline' 'data'"
+                             :img-src "'self' 'data'"}}})
 
 (defn start []
   (server/start (server/create-server service-map)))
@@ -57,19 +63,14 @@
   (reset! server
           (-> service-map ;; start with production configuration
               (merge {:env :dev
-                      ;; do not block thread that starts web server
                       ::server/join? false
-                      ;; Routes can be a function that resolve routes,
-                      ;;  we can use this to set the routes to be reloadable
-                      ;;::server/routes #(deref #'routes)
                       ;; all origins are allowed in dev mode
                       ::server/allowed-origins {:creds true :allowed-origins (constantly true)}})
-              ;; Wire up interceptor chains
-              server/default-interceptors
-              (pedestal/replace-last-interceptor routes)
-              server/dev-interceptors
-              server/create-server
-              server/start)))
+              (server/default-interceptors)
+              (pedestal/replace-last-interceptor router)
+              (server/dev-interceptors)
+              (server/create-server)
+              (server/start))))
 
 (defn stop-dev []
   (server/stop @server))
@@ -82,29 +83,39 @@
   (start-dev)
   (restart)
 
+  (defn to-transit [form]
+    (->> form
+         (m/encode "application/transit+json")
+         (slurp)))
+
+  (test/response-for
+   (:io.pedestal.http/service-fn @server)
+   :post "/repl"
+   :headers {"Content-Type" "application/transit+json"}
+   :body (to-transit '(+ 1 1)))
+
   (def db (disk->edn "resources/got-db.edn"))
-  (test/response-for (:io.pedestal.http/service-fn @lhrb.server/server)
-                     :post "/repl"
-                     :headers {"Content-Type" "application/edn"}
-                     :body "(load-db db \"resources/got-db.edn\")")
 
-  (test/response-for (:io.pedestal.http/service-fn @lhrb.server/server)
-                     :post "/repl"
-                     :headers {"Content-Type" "application/edn"}
-                     :body "(q {:find [?name ?noble]
-      :where [[?b :battle/location \"Storm's End\"]
-              [?b :battle/defender_commander ?name]
-              [?p :char/Name ?name]
-              [?p :char/Nobility ?noble]]}
-     db)")
+  (test/response-for
+   (:io.pedestal.http/service-fn @lhrb.server/server)
+   :post "/repl"
+   :headers {"Content-Type" "application/edn"}
+   :body "(load-db db \"resources/got-db.edn\")")
 
-  (eval '(load-db db2 "resources/got-db.edn"))
+  (test/response-for
+   (:io.pedestal.http/service-fn @lhrb.server/server)
+   :post "/repl"
+   :headers {"Content-Type" "application/transit+json"}
+   :body (to-transit
+          (q {:find [?name ?noble]
+              :where [[?b :battle/location "Storm's End"]
+                      [?b :battle/defender_commander ?name]
+                      [?p :char/Name ?name]
+                      [?p :char/Nobility ?noble]]}
+             db)))
 
-  (test/response-for (:io.pedestal.http/service-fn @lhrb.server/server)
-                     :get "/ping"
-                     :header {"Content-Type" "text/plain"}
-                     :body "hallo")
+  (->> '(+ 1 1)
+       (m/encode "application/transit+json")
+       (m/decode "application/transit+json"))
 
-  (get-in ctx [:request :edn-params])
-
-  ,)
+    *e)
