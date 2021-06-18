e237800e7310adf743c7c830abcc3541b4c8eeb3
lukas <lukas.block@pm.me>
2021-06-18 08:48:00 +0200

    update test cases

--- test/clj/lhrb/replservice_test.clj
+++ test/clj/lhrb/replservice_test.clj
@@ -52,3 +52,108 @@
      {:response {:body {:result #'resp->transit}}})
 
     {:response {:body "[\"^ \",\"~:result\",\"~$resp->transit\"]"}})))
+
+(deftest test-load-db-instructions
+  (is
+   (=
+    ((:enter load-db-instructions)
+     {:body-params
+            '(do
+               (load-db db0 "resources/test-db.edn")
+               (load-db db1 "resources/test-db.edn"))})
+
+    '{:body-params (do),
+     :sym-table
+     {db0
+      [[1 :battle/attacker_commander "Stannis Baratheon"]
+       [1 :battle/defender_1 "Bolton"]
+       [1 :battle/attacker_size 5000]
+       [1 :battle/attacker_4 "Glover"]
+       [1 :battle/defender_king "Joffrey/Tommen Baratheon"]
+       [1 :battle/attacker_2 "Karstark"]
+       [1 :battle/year 300]
+       [1 :battle/name "Siege of Winterfell"]
+       [1 :battle/attacker_3 "Mormont"]
+       [1 :battle/summer false]],
+      db1
+      [[1 :battle/attacker_commander "Stannis Baratheon"]
+       [1 :battle/defender_1 "Bolton"]
+       [1 :battle/attacker_size 5000]
+       [1 :battle/attacker_4 "Glover"]
+       [1 :battle/defender_king "Joffrey/Tommen Baratheon"]
+       [1 :battle/attacker_2 "Karstark"]
+       [1 :battle/year 300]
+       [1 :battle/name "Siege of Winterfell"]
+       [1 :battle/attacker_3 "Mormont"]
+       [1 :battle/summer false]]},
+     :tx-data
+     [[db0
+       [[1 :battle/attacker_commander "Stannis Baratheon"]
+        [1 :battle/defender_1 "Bolton"]
+        [1 :battle/attacker_size 5000]
+        [1 :battle/attacker_4 "Glover"]
+        [1 :battle/defender_king "Joffrey/Tommen Baratheon"]
+        [1 :battle/attacker_2 "Karstark"]
+        [1 :battle/year 300]
+        [1 :battle/name "Siege of Winterfell"]
+        [1 :battle/attacker_3 "Mormont"]
+        [1 :battle/summer false]]]
+      [db1
+       [[1 :battle/attacker_commander "Stannis Baratheon"]
+        [1 :battle/defender_1 "Bolton"]
+        [1 :battle/attacker_size 5000]
+        [1 :battle/attacker_4 "Glover"]
+        [1 :battle/defender_king "Joffrey/Tommen Baratheon"]
+        [1 :battle/attacker_2 "Karstark"]
+        [1 :battle/year 300]
+        [1 :battle/name "Siege of Winterfell"]
+        [1 :battle/attacker_3 "Mormont"]
+        [1 :battle/summer false]]]]})))
+
+(deftest round-trip
+  (is
+   (=
+    (-> {:body-params '(do
+                         (load-db db0 "resources/test-db.edn")
+                         (:a db0))}
+        ((:enter (sym-table-interceptor (atom {:a 'first}))))
+        ((:enter load-db-instructions))
+        ((:enter replace-symbols)))
+    '{:body-params
+      (do
+        (first
+         [[1 :battle/attacker_commander "Stannis Baratheon"]
+          [1 :battle/defender_1 "Bolton"]
+          [1 :battle/attacker_size 5000]
+          [1 :battle/attacker_4 "Glover"]
+          [1 :battle/defender_king "Joffrey/Tommen Baratheon"]
+          [1 :battle/attacker_2 "Karstark"]
+          [1 :battle/year 300]
+          [1 :battle/name "Siege of Winterfell"]
+          [1 :battle/attacker_3 "Mormont"]
+          [1 :battle/summer false]])),
+      :sym-table
+      {:a first,
+       db0
+       [[1 :battle/attacker_commander "Stannis Baratheon"]
+        [1 :battle/defender_1 "Bolton"]
+        [1 :battle/attacker_size 5000]
+        [1 :battle/attacker_4 "Glover"]
+        [1 :battle/defender_king "Joffrey/Tommen Baratheon"]
+        [1 :battle/attacker_2 "Karstark"]
+        [1 :battle/year 300]
+        [1 :battle/name "Siege of Winterfell"]
+        [1 :battle/attacker_3 "Mormont"]
+        [1 :battle/summer false]]},
+      :tx-data
+      [[db0
+        [[1 :battle/attacker_commander "Stannis Baratheon"]
+         [1 :battle/defender_1 "Bolton"]
+         [1 :battle/attacker_size 5000]
+         [1 :battle/attacker_4 "Glover"]
+         [1 :battle/defender_king "Joffrey/Tommen Baratheon"]
+         [1 :battle/attacker_2 "Karstark"]
+         [1 :battle/year 300]
+         [1 :battle/name "Siege of Winterfell"]
+         [1 :battle/attacker_3 "Mormont"]
+         [1 :battle/summer false]]]]})))
