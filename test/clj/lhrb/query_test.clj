edaae96bcb16513826e0d3ee12dc530ddef8c722
lukas <lukas.block@pm.me>
2021-11-03 22:32:26 +0100

    add got db tests

--- test/clj/lhrb/query_test.clj
+++ test/clj/lhrb/query_test.clj
@@ -1,7 +1,8 @@
 (ns lhrb.query-test
   (:require [lhrb.query :as sut :refer [q]]
             [clojure.test :as t]
-            [lhrb.test :refer [is=]]))
+            [lhrb.test :refer [is=]]
+            [lhrb.readcsv :refer [create-got-db]]))
 
 (t/deftest test-q
   (let [db [[0 :name "Peter"]
@@ -29,3 +30,28 @@
     (is= (q {:find [?e]
              :where [[?e :age (> ?a 30)]]} db)
          [[0] [2]])))
+
+(t/deftest got-test
+  (let [db (create-got-db)
+        noble-def-cmd [["Renly Baratheon" "nobel"]
+                       ["Randyll Tarly" "nobel"]
+                       ["Mathis Rowan" "nobel"]
+                       ["Cortnay Penrose" "nobel"]
+                       ["Loras Tyrell" "nobel"]]]
+    (is=
+     "simply test multiple joins"
+     (q {:find [?name ?noble]
+         :where [[?b :battle/location "Storm's End"]
+                 [?b :battle/defender_commander ?name]
+                 [?p :char/Name ?name]
+                 [?p :char/Nobility ?noble]]}
+        db)
+     noble-def-cmd)
+
+    (is=
+     "test 'or' clause. Expect to find two names."
+     (count
+      (q {:find [?e]
+          :where [[?e :char/Name (or "Randa" "Drogo")]]}
+         db))
+     2)))
